c34e26ad18bf9e26de63f11afceb703234f5f344
Felix <github@fbrettnich.de>
2021-04-19 17:27:48 +0200

    Updated SupportPinWidget output

--- modules/addons/supportpin/hooks.php
+++ modules/addons/supportpin/hooks.php
@@ -91,15 +91,15 @@ class SupportPinWidget extends AbstractWidget
 <div class="panel panel-default" data-widget="SupportPinWidget">
     <div class="panel-heading">
         <div class="widget-tools">
-            <a href="/admin/#" class="widget-minimise"><i class="fas fa-chevron-up"></i></a>
-            <a href="/admin/#" class="widget-hide"><i class="fas fa-times"></i></a>
+            <a href="#" class="widget-minimise"><i class="fas fa-chevron-up"></i></a>
+            <a href="#" class="widget-hide"><i class="fas fa-times"></i></a>
         </div>
         <h3 class="panel-title" style="touch-action: none;">Support PIN</h3>
     </div>
     <div class="panel-body">
         <div class="widget-content-padded">
             <div class="text-center">
-                <form action="addonmodules.php?module=supportpin" method="post"  style="text-align: center;">
+                <form action="addonmodules.php?module=supportpin" method="post" style="text-align: center;">
                     <div class="input-group input-group-sm">
                         <input type="number" name="searchsupportpin" class="form-control" required>
                         <div class="input-group-btn">
