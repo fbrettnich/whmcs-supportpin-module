694d601215a6d59bb5153967a5d1260b3e56e276
TutoHacks <50992017+TutoHacks@users.noreply.github.com>
2021-10-05 11:04:55 +0200

    added a new language
    added spanish

---
+++ modules/addons/supportpin/lang/spanish.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * WHMCS-SupportPIN - Let your customers generate a support/phone pin to identify your customers faster, for example on the phone
+ *
+ * Copyright (c) 2021 Felix Brettnich
+ *
+ * This file is part of fbrettnich/whmcs-supportpin-module
+ *
+ * Licensed under GPL-3.0 (https://github.com/fbrettnich/whmcs-supportpin-module/blob/main/LICENSE)
+ */
+
+$_ADDONLANG['client_title'] = "Tu Apoyo-PIN es:";
+$_ADDONLANG['client_info'] = "Tu PIN es valido por tanto tiempo, como si no actualizaste esta pagina.<br>
+Por supuesto, puede salir de esta pagina sin ningun problema; su PIN de soporte no cambiara.";
+$_ADDONLANG['client_regenerate'] = "Genera nuevo PIN";
+
+$_ADDONLANG['admin_title_activepins'] = "Activo PINs las ultimas 48 horas";
+$_ADDONLANG['admin_table_id'] = "Clientes ID";
+$_ADDONLANG['admin_table_firstname'] = "Nombre de pila";
+$_ADDONLANG['admin_table_lastname'] = "Nombre";
+$_ADDONLANG['admin_table_pin'] = "Apoyo PIN";
+$_ADDONLANG['admin_table_updated'] = "Actualizado";
+$_ADDONLANG['admin_table_gocustomer'] = "Al cliente";
